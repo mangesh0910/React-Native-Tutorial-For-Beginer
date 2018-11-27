a8ddad1efa235db8f7a1b9438fde2021b3dc5b7c
Mangesh Khanderao Gosavi <mangeshgosavi300@gmail.com>
2018-11-27 15:28:23 +0530

    border radius used

--- Components/Align.js
+++ Components/Align.js
@@ -6,11 +6,11 @@ export default class Align extends Component {
     return (
       <View style={{flex:1}}>
             <View style={{flex:10,backgroundColor:'blue'}}></View>
-            <View style={{flex:80,backgroundColor:'cyan',flexDirection:'row',alignItems:'center',justifyContent:'space-between',margin:25}}>
-                <View style={{width:52,height:470,backgroundColor:'green',marginLeft:10}}>
+            <View style={{flex:80,backgroundColor:'cyan',flexDirection:'row',alignItems:'center',justifyContent:'space-between',margin:25,borderColor:'gray',borderWidth:25,borderRadius:25}}>
+                <View style={{width:52,height:430,backgroundColor:'green',marginLeft:10}}>
 
                 </View>
-                <View style={{width:52,height:470,backgroundColor:'green',marginRight:10}}>
+                <View style={{width:52,height:430,backgroundColor:'green',marginRight:10}}>
 
 
                 </View>
