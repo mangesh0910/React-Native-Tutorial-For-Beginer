a9c51e9f172925b08b511e3f492216961d04a576
Mangesh Khanderao Gosavi <mangeshgosavi300@gmail.com>
2018-11-27 15:22:13 +0530

    Fixed and Flexed Addes

---
+++ Components/Align.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import {View} from 'react-native'
+
+export default class Align extends Component {
+  render() {
+    return (
+      <View style={{flex:1}}>
+            <View style={{flex:10,backgroundColor:'blue'}}></View>
+            <View style={{flex:80,backgroundColor:'cyan',flexDirection:'row',alignItems:'center',justifyContent:'space-between',margin:25}}>
+                <View style={{width:52,height:470,backgroundColor:'green',marginLeft:10}}>
+
+                </View>
+                <View style={{width:52,height:470,backgroundColor:'green',marginRight:10}}>
+
+
+                </View>
+
+
+
+            </View>
+            <View style={{flex:10,backgroundColor:'blue'}}></View>
+
+      </View>
+    )
+  }
+}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {AppRegistry} from 'react-native';
 
 //import Display3 from './Components/Display3';
 //import MultipleBlink from './Components/MultipleBlink'
-import Design1 from './Components/Design1'
+//import Design1 from './Components/Design1'
+import Align from './Components/Align'
 
-AppRegistry.registerComponent('AwesomeProject', () => Design1);
+AppRegistry.registerComponent('AwesomeProject', () => Align);
